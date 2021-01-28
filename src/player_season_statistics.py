43056e4b3f5d10d9d3712d461e646f784a212b3b
Timothy Lee <timothy.lee821@gmail.com>
2021-01-27 20:27:13 -0800

    need to add catch in cases where player name is not valid or url link is not valid

--- src/player_season_statistics.py
+++ src/player_season_statistics.py
@@ -3,10 +3,28 @@ import requests
 from bs4 import BeautifulSoup
 from pprint import pprint
  
-def scrape_player_stats_2021(): 
-    faker_url = 'https://lol.gamepedia.com/Faker/Statistics/2021' 
-    
-    page = requests.get(faker_url) 
+def player_stats(year):
+    # need to import csv and get player data keys 
+    sample_player_dict = {
+        # 'T1': {'Faker': 'Mid', 'Teddy': 'Bot', 'Canna': 'Jungler'}, 'DWG': {'Showmaker': 'Mid', 'Khan': 'Top'} 
+        'T1': {'Faker': 'Mid'}, 'DWG': {'ShowMaker': 'Mid'}
+        # 'T1': {'Faker': 'Mid'}
+    }
+    player_dict = {} 
+    for team in sample_player_dict.keys():
+        for player in sample_player_dict[team].keys():
+            player_dict[player] = {} 
+            scrape_player_stats_by_year(player, player_dict, year)
+
+    print(player_dict)
+
+def scrape_player_stats_by_year(player, player_dict, year):  
+    player_url = 'https://lol.gamepedia.com/' + player + '/Statistics/' + year
+
+    # faker_url = 'https://lol.gamepedia.com/Faker/Statistics/2021' 
+    # print(player_url)    
+
+    page = requests.get(player_url) 
 
     soup = BeautifulSoup(page.content, 'html.parser')
 
@@ -15,61 +33,60 @@ def scrape_player_stats_2021():
     stats = stats_div.find_all('td')
 
     stat_list = []
+
+    count = 0
     for td in stats:
         stat_list.append(td.text)
+        count += 1
+        if count == 16: 
+            champion_dict = fill_player_champion_stats(stat_list)
+            
+            # update (add layer of dictionary) to each player
+            # every champion should be a new sub entry of the player 
+            # { FAKER : { 'Azir' : ... , 'Zoe': .... } }
+            player_dict[player].update(champion_dict)
+            stat_list.clear()
+            count = 0
 
-    print(len(stat_list))
-    print(stat_list)
+def fill_player_champion_stats(stat_list):
+    temp_dict = {}
+    temp_dict[stat_list[0]] = {}
 
-# Games
-# Win 
-# Loss
-# W/L Ratio
-# Kill
-# Death
-# Assist
-# KDA
-# CS
-# CS/M
-# Gold
-# Gold/M
-# Kill Participation ((K+A) / Team K)
-# Kill Share (K / Team K)
-# Gold Share
+    for count, stat in enumerate(stat_list[1:16]):
+        temp_dict[stat_list[0]][check_index(count)] = stat
+    
+    return temp_dict
 
-# sample output 
-# Azir
-# 4
-# 1
-# 3
-# 25%
-# 2.5
-# 2.75
-# 4.25
-# 2.45
-# 272.5
-# 8.18
-# 12.3
-# 370
-# 65.9%
-# 24.4%
-# 21.7%
-# Zoe
-# 2
-# 1
-# 1
-# 50%
-# 1.5
-# 1.5
-# 5.5
-# 4.67
-# 276.5
-# 7.75
-# 12.3
-# 345
-# 58.3%
-# 12.5%
-# 19.6%
+
+def check_index(index): 
+    item = index % 15
+    mapping = { 
+        0: 'Games',
+        1: 'Win',
+        2: 'Loss',
+        3: 'W/L Ratio',
+        4: 'Kill',
+        5: 'Death',
+        6: 'Assist',
+        7: 'KDA',
+        8: 'CS',
+        9: 'CS/M',
+        10: 'Gold',
+        11: 'Gold/M',
+        12: 'Kill Participation',
+        13: 'Kill Share',
+        14: 'Gold Share',
+    }
+
+    return mapping.get(item)
+
+def test():
+    stat = {} 
+    faker = ['Azir', '4', '1', '3', '25%', '2.5', '2.75', '4.25', '2.45', '272.5', '8.18', '12.3', '370', '65.9%', '24.4%', '21.7%', 'Zoe', '2', '1', '1', '50%', '1.5', '1.5', '5.5', '4.67', '276.5', '7.75', '12.3', '345', '58.3%', '12.5%', '19.6%']
+    # print(make_dict_from_statlist(faker, stat))
+
 
 if __name__ == "__main__":
-    scrape_player_stats_2021()
+    player_stats('2021')
+    # scrape_player_stats_2021()
+    # test()
