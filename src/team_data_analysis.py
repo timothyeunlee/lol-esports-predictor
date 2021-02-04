863a33b22c98313bddee5713605c6e0060330289
Timothy Lee <timothy.lee821@gmail.com>
2021-02-03 21:36:20 -0800

    changes for extracting csvs from directory

--- src/team_data_analysis.py
+++ src/team_data_analysis.py
@@ -6,27 +6,27 @@ def get_team_stats():
     lec = ['SK Gaming', 'Astralis', 'Excel Esports', 'FC Schalke 04 Esports', 'Fnatic', 'G2 Esports', 'MAD Lions', 'Misfits Gaming', 'Rogue', 'Team Vitality'] 
     lck = ['T1', 'Gen.G', 'KT Rolster', 'Afreeca Freecs', 'Nongshim RedForce', 'Liiv SANDBOX', 'Hanwha Life Esports', 'Fredit BRION', 'DWG KIA', 'DRX']
 
-    testcsv = './match_data_csv/2020_LoL_esports_match_data_from_OraclesElixir_20210202.csv'
-    get_team_stats_helper(testcsv, lcs, 'lcs')
-    get_team_stats_helper(testcsv, lec, 'lec')
-    get_team_stats_helper(testcsv, lck, 'lck')
+    # TO RUN SCRIPT DOWNLOAD ALL MATCH DATA FILES FROM 
+    # https://oracleselixir.com/tools/downloads
+    # SAVE IN A DIR 'match_data_csv/'
+    match_data_path = './match_data_csv' 
+    csvs = os.listdir(match_data_path) 
+    for csv in csvs:
+        year = extract_year(csv)
+        get_team_stats_helper(csv, lcs, 'lcs', year)
+        get_team_stats_helper(csv, lec, 'lec', year)
+        get_team_stats_helper(csv, lck, 'lck', year)
 
-    # match_data_path = './match_data_csv' 
-    # csvs = os.listdir(match_data_path) 
-    # for csv in csvs:
-    #     get_team_stats_helper(csv, lcs, 'lcs')
-    #     get_team_stats_helper(csv, lec, 'lec')
-    #     get_team_stats_helper(csv, lck, 'lck')
-
-def get_team_stats_helper(csv, team_list, region): 
-    df = pd.read_csv(csv) 
+def get_team_stats_helper(csv, team_list, region, year): 
+    csv_path = './match_data_csv/' + csv
+    df = pd.read_csv(csv_path) 
     teams_filter = df['position'] == 'team' 
     teams_df = df[teams_filter]
 
     for team in team_list: 
         get_team = teams_df['team'] == team 
         team_df = teams_df[get_team]
-        export_df (team_df, team, region, '2020')
+        export_df (team_df, team, region, year)
 
 def export_df(team_df, team, region, year):
     output_dir = './team_stats_csv'
@@ -43,22 +43,17 @@ def export_df(team_df, team, region, year):
         os.mkdir(region_dir) 
 
     complete_path = os.path.join (region_dir, output_file) 
-    # print (complete_path)
     # print(team_df.shape)
     team_df.to_csv(complete_path)
 
 def format_team(team_name): 
     return team_name.replace(' ', '_').lower()
 
+def extract_year(csv_file):
+    return csv_file[:4]
+
 def main(): 
     get_team_stats()
-    # test2()
 
 if __name__ == "__main__": 
     main() 
-
-
-# team_stats_csv 
-#     - year
-#         - region
-#             - team 
