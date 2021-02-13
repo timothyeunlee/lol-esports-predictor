1a1e86f775c2631477e18a86b889731db9c2e269
Timothy Lee <timothy.lee821@gmail.com>
2021-02-12 19:57:36 -0800

    script to drop columns, concat csvs, export csvs for ml prep

---
+++ src/scripts/team_stats.py
@@ -0,0 +1,44 @@
+import os
+import pandas as pd 
+
+"""
+    SCRIPT to drop unncessary columns from csvs for ml 
+"""
+
+
+# iterates through all csvs in directory 
+# drops columns, concats them together, exports 
+def clean_csv(): 
+    csvs_path = '../data/2020_2021_team_stats_csv'
+    csvs = os.listdir(csvs_path)
+    frames = [] # list of every data frame in the csv dir 
+    for csv in csvs: 
+        full_csv_path = csvs_path + '/' + csv
+        df = pd.read_csv(full_csv_path) 
+        dropped_df = drop_columns(df)
+        frames.append(dropped_df)
+    
+    combined_df = pd.concat(frames)
+    export(combined_df)
+
+# drops unncessary columns 
+# returns new df 
+def drop_columns(df):
+    new_df = df[['result', 'kills', 'deaths', 'dragons', 'elders', 'heralds', 'barons', 'goldat15', 'golddiffat15']]
+    return new_df
+
+def export(df): 
+    output_dir = '../data/teams_ml_data'
+    output_file = 'combined_team_stats.csv'
+
+    if not os.path.exists(output_dir): 
+        os.mkdir(output_dir) 
+
+    complete_path = os.path.join(output_dir, output_file) 
+    df.to_csv(complete_path) 
+
+def main():
+    clean_csv() 
+
+if __name__ == "__main__": 
+    main()
